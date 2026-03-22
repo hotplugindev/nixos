2b0ccf63505adaa4a84beeb648c5788c91698710
Giona Berti <giona7berti@gmail.com>
2026-03-22 17:32:49 +0100

    nixos: switch pc 2026-03-22 17:32:49

--- modules/home/nixvim/core/keymaps.nix
+++ modules/home/nixvim/core/keymaps.nix
@@ -129,44 +129,46 @@
       options.desc = "Create new buffer";
     }
     {
-        mode = "n";
-        key = "<leader>bx";
-        action = {
-            __raw = ''
-            function()
-                local current = vim.api.nvim_get_current_buf()
-                local current_ft = vim.bo[current].filetype
+      mode = "n";
+      key = "<leader>bx";
+      action = {
+        __raw = ''
+          function()
+            local current = vim.api.nvim_get_current_buf()
+            local current_ft = vim.bo[current].filetype
 
-                if current_ft == "neo-tree" then
-                vim.cmd("close")
-                return
-                end
-
-                local listed = vim.fn.getbufinfo({ buflisted = 1 })
-                local target = nil
-                local real_count = 0
-
-                for _, buf in ipairs(listed) do
-                if vim.bo[buf.bufnr].filetype ~= "neo-tree" then
-                    real_count = real_count + 1
-                    if buf.bufnr ~= current and target == nil then
-                    target = buf.bufnr
-                    end
-                end
-                end
-
-                if real_count > 1 and target ~= nil then
-                vim.api.nvim_set_current_buf(target)
-                vim.cmd("bdelete " .. current)
-                else
-                vim.cmd("enew")
-                vim.cmd("bdelete " .. current)
-                end
+            if current_ft == "neo-tree" then
+              vim.cmd("close")
+              return
             end
-            '';
-        };
-        options.desc = "Close buffer";
-        }
+
+            local listed = vim.fn.getbufinfo({ buflisted = 1 })
+            local target = nil
+            local real_count = 0
+
+            for _, buf in ipairs(listed) do
+              if vim.bo[buf.bufnr].filetype ~= "neo-tree" then
+                real_count = real_count + 1
+                if buf.bufnr ~= current and target == nil then
+                  target = buf.bufnr
+                end
+              end
+            end
+
+            if real_count > 1 and target ~= nil then
+              vim.api.nvim_set_current_buf(target)
+              vim.cmd("bdelete " .. current)
+            else
+              vim.cmd("enew")
+              vim.cmd("file [No Name]")
+              vim.cmd("setlocal bufhidden=wipe")
+              vim.cmd("bdelete " .. current)
+            end
+          end
+        '';
+      };
+      options.desc = "Close buffer";
+    }
     {
       mode = "n";
       key = "<leader>bo";
@@ -385,4 +387,3 @@
     }
   ];
 }
-
