94e91d4023d02c60441a71e98c56f9ccc26f2cf1
Giona Berti <giona7berti@gmail.com>
2026-03-22 11:56:41 +0100

    nixos: switch laptop 2026-03-22 11:56:41

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, lib, ... }:
 {
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
@@ -8,4 +8,6 @@
 
   services.power-profiles-daemon.enable = true;
   services.libinput.enable = true;
+
+  services.fwupd.enable = true;
 }
