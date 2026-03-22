7d5e2d16c58504e7088ac8c9b165d51b1a12ba2c
Giona Berti <giona7berti@gmail.com>
2026-03-22 11:31:25 +0100

    testing

--- modules/home/desktop/foot.nix
+++ modules/home/desktop/foot.nix
@@ -1,5 +1,6 @@
-{ ... }:
+{ hostType, ... }:
 {
   programs.foot.enable = true;
-  xdg.configFile."foot/foot.ini".source = ../../../config/foot/foot.ini;
+  (if hostType == "laptop" then [ ./laptop.nix ] else [ ./pc.nix ]);
+  xdg.configFile."foot/foot.ini".source = (if hostType == "laptop" then [ ../../../config/foot/foot-laptop.ini ] else [ ../../../config/foot/foot-pc.ini ]);
 }
