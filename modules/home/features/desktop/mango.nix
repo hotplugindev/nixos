4f64a767e96fd5dab618b32c07d023603f680de4
Giona Berti <giona7berti@gmail.com>
2026-03-22 21:30:19 +0100

    nixos: switch pc 2026-03-22 21:30:19

--- modules/home/features/desktop/mango.nix
+++ modules/home/features/desktop/mango.nix
@@ -1,4 +1,10 @@
-{ inputs, pkgs, lib, hostType, ... }:
+{
+  inputs,
+  pkgs,
+  lib,
+  hostType,
+  ...
+}:
 {
   imports = [ inputs.mango.hmModules.mango ];
 
@@ -8,44 +14,54 @@
     autostart_sh = builtins.readFile ../../config/mango/autostart.sh;
   };
 
-systemd.user.services.polkit-gnome-authentication-agent-1 = {
-  Unit = {
-    Description = "polkit-gnome-authentication-agent-1";
-    Wants = [ "graphical-session.target" ];
-    After = [ "graphical-session.target" ];
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    Unit = {
+      Description = "polkit-gnome-authentication-agent-1";
+      Wants = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
   };
-  Install = {
-    WantedBy = [ "graphical-session.target" ];
-  };
-  Service = {
-    Type = "simple";
-    ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-    Restart = "on-failure";
-    RestartSec = 1;
-    TimeoutStopSec = 10;
-  };
-};
 
-  home.packages = with pkgs; [
-    foot
-    grim
-    slurp
-    brightnessctl
-    pamixer
-    playerctl
-    nautilus
-    wofi
-    swaylock
-    qt6Packages.qt6ct
-    wlogout
-    polkit_gnome
-    swaynotificationcenter
-    sox
-  ] ++ lib.optionals (hostType == "laptop") [ wlr-randr ];
+  home.packages =
+    with pkgs;
+    [
+      foot
+      grim
+      slurp
+      brightnessctl
+      pamixer
+      playerctl
+      nautilus
+      wofi
+      swaylock
+      qt6Packages.qt6ct
+      wlogout
+      polkit_gnome
+      swaynotificationcenter
+      sox
+    ]
+    ++ lib.optionals (hostType == "laptop") [ wlr-randr ];
 
   xdg.configFile."mango/config.conf".source = ../../config/mango/config.conf;
   xdg.configFile."mango/bind.conf".source = ../../config/mango/bind.conf;
   xdg.configFile."mango/env.conf".source = ../../config/mango/env.conf;
   xdg.configFile."mango/rule.conf".source = ../../config/mango/rule.conf;
   xdg.configFile."mango/tag.conf".source = ../../config/mango/tag.conf;
+  xdg.configFile."mango/monitor.conf".source =
+    if hostType == "laptop" then
+      ../../config/mango/monitor-laptop.conf
+    else
+      ../../config/mango/monitor-pc.conf;
+
+  xdg.configFile."wallpaper".source = ../../config/wallpaper;
 }
