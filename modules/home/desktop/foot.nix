ef26348472a48d048213e65f932458969c5cf3ef
Giona Berti <giona7berti@gmail.com>
2026-03-22 11:33:44 +0100

    nixos: switch pc 2026-03-22 11:33:44

--- modules/home/desktop/foot.nix
+++ modules/home/desktop/foot.nix
@@ -1,6 +1,5 @@
 { hostType, ... }:
 {
   programs.foot.enable = true;
-  (if hostType == "laptop" then [ ./laptop.nix ] else [ ./pc.nix ]);
-  xdg.configFile."foot/foot.ini".source = (if hostType == "laptop" then [ ../../../config/foot/foot-laptop.ini ] else [ ../../../config/foot/foot-pc.ini ]);
+  xdg.configFile."foot/foot.ini".source = if hostType == "laptop" then ../../../config/foot/foot-laptop.ini else ../../../config/foot/foot-pc.ini;
 }
