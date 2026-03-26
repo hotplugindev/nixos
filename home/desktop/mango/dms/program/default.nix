79bc96b85dcc5bf46fdba4b5f558b76e8f815c22
Giona Berti <giona7berti@gmail.com>
2026-03-26 20:26:49 +0100

    nixos: switch laptop 2026-03-26 20:26:49

--- home/desktop/mango/dms/program/default.nix
+++ home/desktop/mango/dms/program/default.nix
@@ -23,6 +23,59 @@ in
         currentThemeName = "dynamic";
         currentThemeCategory = "dynamic";
         matugenScheme = "scheme-content";
+
+        controlCenterWidgets = [
+          {
+            id = "volumeSlider";
+            enabled = true;
+            width = 50;
+          }
+          {
+            id = "brightnessSlider";
+            enabled = true;
+            width = 50;
+          }
+          {
+            id = "wifi";
+            enabled = true;
+            width = 50;
+          }
+          {
+            id = "bluetooth";
+            enabled = true;
+            width = 50;
+          }
+          {
+            id = "audioOutput";
+            enabled = true;
+            width = 50;
+          }
+          {
+            id = "audioInput";
+            enabled = true;
+            width = 50;
+          }
+          {
+            id = "nightMode";
+            enabled = true;
+            width = 50;
+          }
+          {
+            id = "darkMode";
+            enabled = true;
+            width = 50;
+          }
+          {
+            id = "builtin_vpn";
+            enabled = true;
+            width = 50;
+          }
+          {
+            id = "colorPicker";
+            enabled = true;
+            width = 50;
+          }
+        ];
       };
       session = {
         weatherLocation = lib.mkDefault "Brixen - Bressanone, 39042";
