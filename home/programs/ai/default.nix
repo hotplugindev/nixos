c5bcb7a9dcdc129ed48f37659babe5daffcaa360
Giona Berti <giona7berti@gmail.com>
2026-03-26 19:08:41 +0100

    nixos: switch pc 2026-03-26 19:08:41

--- home/programs/ai/default.nix
+++ home/programs/ai/default.nix
@@ -7,8 +7,8 @@
     ./llama-cpp.nix
   ];
 
+  gb.home.programs.ai.llama-cpp.enable = lib.mkDefault true;
   gb.home.programs.ai.codex.enable = lib.mkDefault false;
   gb.home.programs.ai.claude-code.enable = lib.mkDefault false;
   gb.home.programs.ai.gemini-cli.enable = lib.mkDefault false;
-  gb.home.programs.ai.llama-cpp.enable = lib.mkDefault false;
 }
