531f3dfdcf8b5f8f2bc1446714cf89dfbc95e85c
Giona Berti <giona7berti@gmail.com>
2026-03-27 18:15:47 +0100

    nixos(pc): switch 2026-03-27 18:15:47

--- home/pc/default.nix
+++ home/pc/default.nix
@@ -8,7 +8,7 @@
   gb.home.programs.gaming.boxflat.enable = true;
 
   gb.home.desktop.mango.mangowc.monitors = [
-    "name:HDMI-A-1,width:1920,height:1080,refresh:60,x:0,y:0,scale:1"
     "name:DP-2,width:2560,height:1440,refresh:240,x:1920,y:0,scale:1"
+    "name:HDMI-A-1,width:1920,height:1080,refresh:60,x:0,y:0,scale:1"
   ];
 }
