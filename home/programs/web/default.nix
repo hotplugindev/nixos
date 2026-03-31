db971d602f99a0c3b4dbe8b332620c29909388df
Giona Berti <giona7berti@gmail.com>
2026-03-31 07:48:52 +0200

    nixos(laptop): switch 2026-03-31 07:48:52

--- home/programs/web/default.nix
+++ home/programs/web/default.nix
@@ -4,9 +4,12 @@
     ./brave.nix
     ./firefox.nix
     ./google-chrome.nix
+    ./librewolf.nix
   ];
 
   gb.home.programs.web.brave.enable = lib.mkDefault true;
-  gb.home.programs.web.firefox.enable = lib.mkDefault true;
-  gb.home.programs.web.google-chrome.enable = lib.mkDefault true;
+  gb.home.programs.web.librewolf.enable = lib.mkDefault false;
+
+  gb.home.programs.web.firefox.enable = lib.mkDefault false;
+  gb.home.programs.web.google-chrome.enable = lib.mkDefault false;
 }
