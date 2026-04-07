46504f29f2db0d843c7b101bc8ccc1f18ddd1ee9
Giona Berti <giona7berti@gmail.com>
2026-04-07 12:56:18 +0200

    nixos(pc): switch 2026-04-07 12:56:18

--- home/pc/default.nix
+++ home/pc/default.nix
@@ -6,6 +6,7 @@
   gb.home.programs.gaming.steampack.enable = true;
   gb.home.programs.gaming.bottles.enable = true;
   gb.home.programs.gaming.boxflat.enable = true;
+  gb.home.programs.gaming.ckan.enable = true;
 
   gb.home.desktop.mango.mangowc.monitors = [
     "name:DP-2,width:1920,height:1080,refresh:240,x:1920,y:0,scale:1"
--- home/programs/gaming/default.nix
+++ home/programs/gaming/default.nix
@@ -4,9 +4,11 @@
     ./steampack.nix
     ./bottles.nix
     ./boxflat.nix
+    ./ckan-mod-manager.nix
   ];
 
   gb.home.programs.gaming.steampack.enable = lib.mkDefault false;
   gb.home.programs.gaming.bottles.enable = lib.mkDefault false;
   gb.home.programs.gaming.boxflat.enable = lib.mkDefault false;
+  gb.home.programs.gaming.ckan.enable = lib.mkDefault false;
 }
