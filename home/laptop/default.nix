336f735efe50ab585167358c830021b24b84bd7f
Giona Berti <giona7berti@gmail.com>
2026-04-10 10:39:33 +0200

    nixos(laptop): switch 2026-04-10 10:39:33

--- home/laptop/default.nix
+++ home/laptop/default.nix
@@ -21,4 +21,6 @@
   gb.home.desktop.mango.mangowc.monitors = [
     "name:eDP-1,width:2256,height:1504,refresh:60,x:0,y:10,scale:1.333,vrr:0,rr:0"
   ];
+
+  gb.home.dev.languages.python.libraries = ps: [ ps.pytest ];
 }
