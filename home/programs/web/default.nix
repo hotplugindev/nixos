0f7e9312fc8e5b00751439147111b02bfae17b62
Giona Berti <giona7berti@gmail.com>
2026-04-21 07:58:15 +0200

    nixos(laptop): switch 2026-04-21 07:58:15

--- home/programs/web/default.nix
+++ home/programs/web/default.nix
@@ -5,10 +5,12 @@
     ./firefox.nix
     ./google-chrome.nix
     ./librewolf.nix
+    ./torbrowser.nix
   ];
 
   gb.home.programs.web.brave.enable = lib.mkDefault true;
   gb.home.programs.web.firefox.enable = lib.mkDefault true;
+  gb.home.programs.web.torbrowser.enable = lib.mkDefault true;
 
   gb.home.programs.web.librewolf.enable = lib.mkDefault false;
   gb.home.programs.web.google-chrome.enable = lib.mkDefault false;
