13c767beabb410d4bcdfce07207bb92cc616655a
Giona Berti <giona7berti@gmail.com>
2026-05-08 22:49:13 +0200

    nixos(pc): switch 2026-05-08 22:49:13

--- home/dev/languages/default.nix
+++ home/dev/languages/default.nix
@@ -9,6 +9,7 @@
     ./zig.nix
     ./python.nix
     ./php.nix
+    ./flutter.nix
   ];
 
   gb.home.dev.languages.node.enable = lib.mkDefault true;
@@ -17,6 +18,7 @@
   gb.home.dev.languages.python.enable = lib.mkDefault true;
 
   gb.home.dev.languages.dotnet.enable = lib.mkDefault false;
+  gb.home.dev.languages.flutter.enable = lib.mkDefault false;
   gb.home.dev.languages.php.enable = lib.mkDefault false;
   gb.home.dev.languages.zig.enable = lib.mkDefault false;
   gb.home.dev.languages.go.enable = lib.mkDefault false;
