7ece11b353d0e12196ab6cbd9c30143ca78e6e52
Giona Berti <giona7berti@gmail.com>
2026-05-09 00:45:00 +0200

    nixos(pc): switch 2026-05-09 00:45:00

--- home/programs/communication/default.nix
+++ home/programs/communication/default.nix
@@ -2,7 +2,9 @@
 {
   imports = [
     ./discord.nix
+    ./element.nix
   ];
 
   gb.home.programs.communication.discord.enable = lib.mkDefault true;
+  gb.home.programs.communication.element.enable = lib.mkDefault true;
 }
