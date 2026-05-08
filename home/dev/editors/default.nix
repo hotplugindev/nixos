47c29715421cbacf13ea09e98186bfce13b51c7f
Giona Berti <giona7berti@gmail.com>
2026-05-08 23:43:51 +0200

    nixos(pc): switch 2026-05-08 23:43:51

--- home/dev/editors/default.nix
+++ home/dev/editors/default.nix
@@ -4,10 +4,12 @@
     ./vscode.nix
     ./zed.nix
     ./android-studio.nix
+    ./antigravity.nix
   ];
 
   gb.home.dev.editors.vscode.enable = lib.mkDefault true;
   gb.home.dev.editors.zed.enable = lib.mkDefault true;
 
   gb.home.dev.editors.android-studio.enable = lib.mkDefault false;
+  gb.home.dev.editors.antigravity.enable = lib.mkDefault false;
 }
--- home/pc/default.nix
+++ home/pc/default.nix
@@ -13,6 +13,7 @@
   gb.home.dev.languages.flutter.enable = true;
 
   gb.home.dev.editors.android-studio.enable = true;
+  gb.home.dev.editors.antigravity.enable = true;
 
   gb.home.desktop.mango.mangowc.monitors = [
     "name:DP-2,width:1920,height:1080,refresh:240,x:1920,y:0,scale:1"
