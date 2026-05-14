f700b36a30dcc8b115f4443901b8e2760d5b5b88
Giona Berti <giona7berti@gmail.com>
2026-05-14 19:35:13 +0200

    nixos(pc): switch 2026-05-14 19:35:13

---
+++ modules/desktop/tuigreet.nix
@@ -0,0 +1,19 @@
+{ lib, config, pkgs, ... }:
+
+let
+  tuigreetCfg = config.gb.desktop.tuigreet;
+in
+{
+  options.gb.desktop.tuigreet.enable =
+    lib.mkEnableOption "Enable tuigreet";
+
+  config = lib.mkIf tuigreetCfg.enable {
+    services.greetd = {
+      enable = true;
+      settings.default_session = {
+        user = "greeter";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd mango";
+      };
+    };
+  };
+}
