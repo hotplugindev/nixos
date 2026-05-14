42dd1fa0acc295c00fd1e0caac7de1b4ee7e9a8b
Giona Berti <giona7berti@gmail.com>
2026-05-14 19:31:57 +0200

    nixos(pc): switch 2026-05-14 19:31:57

--- modules/desktop/regreet.nix
+++ modules/desktop/regreet.nix
@@ -10,16 +10,12 @@ in
   config = lib.mkIf regreetCfg.enable {
     programs.regreet.enable = true;
 
-    services.greetd.settings.default_session = {
-      user = "greeter";
-      command = "${pkgs.sway}/bin/sway --config /etc/greetd/sway-config";
+    services.greetd = {
+      enable = true;
+      settings.default_session = {
+        user = "greeter";
+        command = "${pkgs.mangowc}/bin/mango -s '${config.programs.regreet.package}/bin/regreet'";
+      };
     };
-
-    environment.etc."greetd/sway-config".text = ''
-      output DP-2 position 1920 0
-      output HDMI-A-1 position 0 0
-
-      exec "${config.programs.regreet.package}/bin/regreet; ${pkgs.sway}/bin/swaymsg exit"
-    '';
   };
 }
