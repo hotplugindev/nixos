274ac88a0922809031f234943627feb21e019b3e
Giona Berti <giona7berti@gmail.com>
2026-05-14 21:22:42 +0200

    nixos(pc): switch 2026-05-14 21:22:42

--- modules/desktop/tuigreet.nix
+++ modules/desktop/tuigreet.nix
@@ -1,35 +1,18 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}:
+{ lib, config, pkgs, ... }:
 
 let
   tuigreetCfg = config.gb.desktop.tuigreet;
 in
 {
-  options = {
-    gb.desktop.tuigreet.enable = lib.mkEnableOption "Enable tuigreet";
-  };
+  options.gb.desktop.tuigreet.enable =
+    lib.mkEnableOption "Enable tuigreet";
 
   config = lib.mkIf tuigreetCfg.enable {
     services.greetd = {
       enable = true;
-
-      settings = {
-        default_session = {
-          user = "greeter";
-          command = ''
-            ${pkgs.tuigreet}/bin/tuigreet \
-              --time \
-              --remember \
-              --remember-user-session \
-              --user-menu \
-              --asterisks \
-              --cmd ${pkgs.mangowc}/bin/mango
-          '';
-        };
+      settings.default_session = {
+        user = "greeter";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'zsh -l -c mango'";
       };
     };
   };
