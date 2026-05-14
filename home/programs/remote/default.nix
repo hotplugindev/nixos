041915d4e50c874da49b606bcf80983b805cbcf2
Giona Berti <giona7berti@gmail.com>
2026-05-14 18:24:19 +0200

    nixos(pc): switch 2026-05-14 18:24:19

--- home/programs/remote/default.nix
+++ home/programs/remote/default.nix
@@ -3,10 +3,10 @@
   imports = [
     ./remmina.nix
     ./tigervnc.nix
-    ./moonshine.nix
+    ./moonlight-qt.nix
   ];
 
   gb.home.programs.remote.remmina.enable = lib.mkDefault false;
   gb.home.programs.remote.tigervnc.enable = lib.mkDefault false;
-  gb.home.programs.remote.moonshine.enable = lib.mkDefault false;
+  gb.home.programs.remote.moonlight-qt.enable = lib.mkDefault false;
 }
