62f316d8bf11138575f3f587c039c0bd14697946
Giona Berti <giona7berti@gmail.com>
2026-05-14 19:35:37 +0200

    nixos(pc): switch 2026-05-14 19:35:37

--- modules/desktop/tuigreet.nix
+++ modules/desktop/tuigreet.nix
@@ -12,7 +12,7 @@ in
       enable = true;
       settings.default_session = {
         user = "greeter";
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd mango";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd mango";
       };
     };
   };
