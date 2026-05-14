95d4d1b9870a738d9d2ad1e3c04ea699d8e8768f
Giona Berti <giona7berti@gmail.com>
2026-05-14 19:23:30 +0200

    nixos(pc): switch 2026-05-14 19:23:30

--- modules/desktop/regreet.nix
+++ modules/desktop/regreet.nix
@@ -1,12 +1,25 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
+
 let
   regreetCfg = config.gb.desktop.regreet;
 in
 {
-  options = {
-    gb.desktop.regreet.enable = lib.mkEnableOption "Enable regreet";
-  };
+  options.gb.desktop.regreet.enable =
+    lib.mkEnableOption "Enable regreet";
+
   config = lib.mkIf regreetCfg.enable {
     programs.regreet.enable = true;
+
+    services.greetd.settings.default_session = {
+      user = "greeter";
+      command = "${pkgs.sway}/bin/sway --config /etc/greetd/sway-config";
+    };
+
+    environment.etc."greetd/sway-config".text = ''
+      output DP-2 position 1920 0
+      output HDMI-A-1 position 0 0
+
+      exec "${config.programs.regreet.package}/bin/regreet; ${pkgs.sway}/bin/swaymsg exit"
+    '';
   };
 }
