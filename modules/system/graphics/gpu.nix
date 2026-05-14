acab09c48e8996d8812ecb3b2c18548b84decdd1
Giona Berti <giona7berti@gmail.com>
2026-05-14 22:21:33 +0200

    nixos(pc): switch 2026-05-14 22:21:33

--- modules/system/graphics/gpu.nix
+++ modules/system/graphics/gpu.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   gpu = config.gb.system.graphics.gpu;
 in
@@ -7,9 +12,15 @@ in
     gb.system.graphics.gpu.enable = lib.mkEnableOption "Enable hardware Graphics for the system";
   };
   config = {
+    environment.variables = {
+      RUSTICL_ENABLE = "radeonsi";
+    };
     hardware.graphics = lib.mkIf gpu.enable {
       enable = true;
       enable32Bit = true;
+      extraPackages = with pkgs; [
+        mesa.opencl # Enables Rusticl (OpenCL) support
+      ];
     };
   };
 }
