5dcfdf4d2d1a0c11d81372d9eb6158b78b417451
Giona Berti <giona7berti@gmail.com>
2026-05-14 21:28:09 +0200

    nixos(pc): switch 2026-05-14 21:28:09

--- modules/desktop/tuigreet.nix
+++ modules/desktop/tuigreet.nix
@@ -1,18 +1,48 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  username,
+  ...
+}:
 
 let
   tuigreetCfg = config.gb.desktop.tuigreet;
 in
 {
-  options.gb.desktop.tuigreet.enable =
-    lib.mkEnableOption "Enable tuigreet";
+  options = {
+    gb.desktop.tuigreet.enable =
+      lib.mkEnableOption "Enable tuigreet";
+
+    gb.desktop.tuigreet.autologin =
+      lib.mkEnableOption "Enable greetd initial auto-login";
+  };
 
   config = lib.mkIf tuigreetCfg.enable {
+    security.pam.services.greetd.enableGnomeKeyring = true;
+    services.gnome.gnome-keyring.enable = true;
+
     services.greetd = {
       enable = true;
-      settings.default_session = {
-        user = "greeter";
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'zsh -l -c mango'";
+
+      settings = {
+        initial_session = lib.mkIf tuigreetCfg.autologin {
+          user = username;
+          command = "zsh -l -c mango";
+        };
+
+        default_session = {
+          user = "greeter";
+          command = ''
+            ${pkgs.tuigreet}/bin/tuigreet \
+              --time \
+              --remember \
+              --remember-user-session \
+              --user-menu \
+              --asterisks \
+              --cmd "zsh -l -c mango"
+          '';
+        };
       };
     };
   };
