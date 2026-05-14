d52d1af8709e3470ae5b245ea7c4ede1003908f7
Giona Berti <giona7berti@gmail.com>
2026-05-14 18:12:06 +0200

    nixos(pc): switch 2026-05-14 18:12:06

--- home/programs/productivity/default.nix
+++ home/programs/productivity/default.nix
@@ -7,6 +7,7 @@
     ./nextcloud.nix
     ./bitwarden.nix
     ./shotcut.nix
+    ./davinci-resolve.nix
   ];
 
   gb.home.programs.productivity.obsidian.enable = lib.mkDefault true;
@@ -16,5 +17,6 @@
   gb.home.programs.productivity.nextcloud.enable = lib.mkDefault true;
   gb.home.programs.productivity.bitwarden.enable = lib.mkDefault true;
 
+  gb.home.programs.productivity.davinci-resolve.enable = lib.mkDefault false;
   gb.home.programs.productivity.shotcut.enable = lib.mkDefault false;
 }
