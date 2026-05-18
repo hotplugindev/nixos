f0a53fefd03dac0cbf00b98ca6a3358285596c0d
Giona Berti <giona7berti@gmail.com>
2026-05-18 11:18:28 +0200

    nixos(laptop): switch 2026-05-18 11:18:27

--- home/tools/packagemanagement/default.nix
+++ home/tools/packagemanagement/default.nix
@@ -2,9 +2,7 @@
 {
   imports = [
     ./pkg-config.nix
-    ./dbus.nix
   ];
 
   gb.home.tools.packagemanagement.pkg-config.enable = lib.mkDefault true;
-  gb.home.tools.packagemanagement.dbus.enable = lib.mkDefault true;
 }
