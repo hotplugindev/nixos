874204caa95e87aa85791f7c18797c2725b5fdfe
Giona Berti <giona7berti@gmail.com>
2026-05-19 17:04:47 +0200

    nixos(pc): switch 2026-05-19 17:04:47

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -7,11 +7,13 @@
     ./xdg.nix
     ./dconf.nix
     ./gnome-keyring.nix
+    ./xwayland.nix
   ];
   gb.desktop.tuigreet.enable = lib.mkDefault true;
   gb.desktop.mango.enable = lib.mkDefault true;
   gb.desktop.audio.enable = lib.mkDefault true;
   gb.desktop.xdg.enable = lib.mkDefault true;
   gb.desktop.dconf.enable = lib.mkDefault true;
+  gb.desktop.xwayland.enable = lib.mkDefault true;
   gb.desktop.gnome-keyring.enable = lib.mkDefault true;
 }
---
+++ modules/desktop/xwayland.nix
@@ -0,0 +1,12 @@
+{ lib, config, ... }:
+let
+  xwayland = config.gb.desktop.xwayland;
+in
+{
+  options = {
+    gb.desktop.xwayland.enable = lib.mkEnableOption "Enable xwayland";
+  };
+  config = lib.mkIf xwayland.enable {
+    programs.xwayland.enable = true;
+  };
+}
