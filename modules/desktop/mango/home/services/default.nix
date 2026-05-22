bdce51c437ef78f4330ceb333acc7d56fc53391f
Giona Berti <mail@gberti.com>
2026-05-22 10:02:08 +0200

    nixos(laptop): switch 2026-05-22 10:02:08

--- modules/desktop/mango/home/services/default.nix
+++ modules/desktop/mango/home/services/default.nix
@@ -16,7 +16,7 @@ in
     gb.home.desktop.mango.services.sessionTarget.enable = lib.mkDefault mango.services.enable;
     gb.home.desktop.mango.services.authAgent.enable = lib.mkDefault mango.services.enable;
     gb.home.desktop.mango.services.dbusEnv.enable = lib.mkDefault mango.services.enable;
-    gb.home.desktop.mango.services.cliphist.enable = lib.mkDefault mango.services.enable;
+    gb.home.desktop.mango.services.cliphist.enable = lib.mkDefault false; # mango.services.enable;
     gb.home.desktop.mango.services.nextcloud.enable = lib.mkDefault mango.services.enable;
   };
 }
