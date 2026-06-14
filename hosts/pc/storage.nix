52d1e61e658d2f4891dad1354e7f102ac281fd99
Giona Berti <mail@gberti.com>
2026-06-14 18:27:33 +0200

    nixos(pc): switch 2026-06-14 18:27:33

--- hosts/pc/storage.nix
+++ hosts/pc/storage.nix
@@ -1,15 +1,5 @@
 { config, ... }:
 {
-  fileSystems."/mnt/fun" = {
-    device = "/dev/disk/by-uuid/f810e5a8-83ac-44f2-94d3-38d9be18a29c";
-    fsType = "auto";
-    options = [
-      "defaults"
-      "nofail"
-      "x-systemd.device-timeout=5s"
-      "x-gvfs-show"
-    ];
-  };
   fileSystems."/mnt/work" = {
     device = "/dev/disk/by-uuid/1b587ad2-a133-44c9-9236-41099c41bece";
     fsType = "auto";
