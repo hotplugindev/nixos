745d3cc200207394c9bac7cee010b5766005f1ca
Giona Berti <mail@gberti.com>
2026-07-26 21:26:29 +0200

    nixos(pc): switch 2026-07-26 21:26:29

--- modules/programs/home/tools/audiomanagement/default.nix
+++ modules/programs/home/tools/audiomanagement/default.nix
@@ -2,7 +2,9 @@
 {
   imports = [
     ./pavucontrol.nix
+    ./noisetorch.nix
   ];
 
   gb.home.tools.audiomanagement.pavucontrol.enable = lib.mkDefault true;
+  gb.home.tools.audiomanagement.noisetorch.enable = lib.mkDefault false;
 }
