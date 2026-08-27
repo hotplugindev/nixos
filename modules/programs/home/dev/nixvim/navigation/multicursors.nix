f59309710973b9f80a654915b4b6bfc9d1f14985
Giona Berti <mail@gberti.com>
2026-08-27 13:28:30 +0200

    nixos(laptop): switch 2026-08-27 13:28:30

---
+++ modules/programs/home/dev/nixvim/navigation/multicursors.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  multicursors = config.gb.home.dev.nixvim.navigation.multicursors;
+in
+{
+  options = {
+    gb.home.dev.nixvim.navigation.multicursors.enable = lib.mkEnableOption "Enable nixvim multicursors";
+  };
+
+  config = lib.mkIf multicursors.enable {
+    programs.nixvim.plugins.multicursors = {
+      enable = true;
+
+      settings = {
+        DEBUG_MODE = false;
+        create_commands = true;
+
+        hint_config = {
+          position = "top";
+          type = "cmdline";
+        };
+      };
+    };
+  };
+}
