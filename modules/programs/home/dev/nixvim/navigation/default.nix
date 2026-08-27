6c77fb767b42d960e5b78b0c766ad0120f20ccbb
Giona Berti <mail@gberti.com>
2026-08-27 13:28:30 +0200

    nixos(laptop): switch 2026-08-27 13:28:30

--- modules/programs/home/dev/nixvim/navigation/default.nix
+++ modules/programs/home/dev/nixvim/navigation/default.nix
@@ -3,8 +3,10 @@
   imports = [
     ./explorer.nix
     ./telescope.nix
+    ./multicursors.nix
   ];
 
   gb.home.dev.nixvim.navigation.explorer.enable = lib.mkDefault true;
   gb.home.dev.nixvim.navigation.telescope.enable = lib.mkDefault true;
+  gb.home.dev.nixvim.navigation.multicursors.enable = lib.mkDefault true;
 }
