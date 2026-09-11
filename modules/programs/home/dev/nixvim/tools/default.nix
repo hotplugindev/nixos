20f13735ee54f7abc33ba97be77b1ebd3616ccab
Giona Berti <mail@gberti.com>
2026-09-11 08:39:22 +0200

    nixos(laptop): switch 2026-09-11 08:39:22

--- modules/programs/home/dev/nixvim/tools/default.nix
+++ modules/programs/home/dev/nixvim/tools/default.nix
@@ -5,7 +5,6 @@
     ./neogit.nix
     ./diffview.nix
     ./gitlinker.nix
-    ./gh-nvim.nix
     ./avante.nix
   ];
 
@@ -13,6 +12,5 @@
   gb.home.dev.nixvim.tools.neogit.enable = lib.mkDefault true;
   gb.home.dev.nixvim.tools.diffview.enable = lib.mkDefault true;
   gb.home.dev.nixvim.tools.gitlinker.enable = lib.mkDefault true;
-  gb.home.dev.nixvim.tools.gh-nvim.enable = lib.mkDefault false;
   gb.home.dev.nixvim.tools.avante.enable = lib.mkDefault false;
 }
