afd9d9bdcce176a8a7e3467de5a1e8b29cf2c96c
Giona Berti <mail@gberti.com>
2026-09-15 17:21:02 +0200

    nixos(pc): switch 2026-09-15 17:21:02

--- hosts/pc/storage.nix
+++ hosts/pc/storage.nix
@@ -1,16 +1,3 @@
 { config, ... }:
 {
-  fileSystems."/mnt/work" = {
-    device = "/dev/disk/by-uuid/1b587ad2-a133-44c9-9236-41099c41bece";
-    fsType = "auto";
-    options = [
-      "defaults"
-      "nofail"
-      "x-systemd.device-timeout=5s"
-      "x-gvfs-show"
-    ];
-  };
-  systemd.tmpfiles.rules = [
-    "d /mnt/work 0755 ${config.gb.user.username} users -"
-  ];
 }
